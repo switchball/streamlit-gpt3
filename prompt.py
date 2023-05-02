7ee03842fdd6a61f24a22bca96c04c3f4c05b6d4
switchball <xyc5000@hotmail.com>
2023-05-02 15:14:12 +0800

    update a new emoji translator prompt

--- prompt.py
+++ prompt.py
@@ -70,6 +70,13 @@ PROMPTS = [
         "input": "你还有其他问题需要解答吗？"
     },
     {
+        "preset": "标题转emoji (Fun)",
+        "prompt": "Translate the title with emojis ONLY, NO characters",
+        "message": [
+        ],
+        "input": "<输入文章标题>"
+    },
+    {
         "preset": "Android 开发工程师面试",
         "prompt": "我想让你担任 Android 开发工程师面试官。我将成为候选人，您将向我询问 Android 开发工程师职位的面试问题。我希望你只作为面试官回答。不要一次写出所有的问题。我希望你只对我进行采访。问我问题，等待我的回答。不要写解释。像面试官一样一个一个问我，等我回答。我的第一句话是“面试官你好”",
         "message": [
