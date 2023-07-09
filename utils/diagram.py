7698e5e4b901f6c9e1eb9ee2cf8a56294f2d057a
switchball <xyc5000@hotmail.com>
2023-07-09 23:13:21 +0800

    improve encoding

--- utils/diagram.py
+++ utils/diagram.py
@@ -5,12 +5,12 @@ def deflate_and_base64_encode(data):
     # 使用zlib进行deflate压缩
     compressed_data = zlib.compress(data, level=9)
     # 使用base64进行编码
-    encoded_data = base64.b64encode(compressed_data)
+    encoded_data = base64.urlsafe_b64encode(compressed_data)
     return encoded_data
 
 def base64_decode_and_inflate(encoded_data):
     # 使用base64进行解码
-    compressed_data = base64.b64decode(encoded_data)
+    compressed_data = base64.urlsafe_b64decode(encoded_data)
     # 使用zlib进行inflate解压缩
     data = zlib.decompress(compressed_data)
     return data
@@ -23,5 +23,5 @@ def get_common_diagram(text, diagram_type, image_format="svg"):
         text = text.encode('utf-8')
     original_data = text
     encoded_data = deflate_and_base64_encode(original_data)
-    url_param = encoded_data.decode("utf-8").replace("/", "_").replace("+", "-")
+    url_param = encoded_data.decode("ascii")
     return f"https://kroki.io/{diagram_type}/{image_format}/{url_param}"
